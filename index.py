4f55a74060087b91f1f948dc186c82b4bea5b19d
gesse-fernandes <gesse-fm@hotmail.com>
2023-01-27 10:20:51 -0300

    interpretando comandos com a IA do python

--- index.py
+++ index.py
@@ -1,16 +1,20 @@
 import speech_recognition as sr
+import re
 
-mic = sr.Recognizer()
+while True:
+    mic = sr.Recognizer()
 
-with sr.Microphone() as source:
-        mic.adjust_for_ambient_noise(source=source)
+    with sr.Microphone() as source:
+            mic.adjust_for_ambient_noise(source=source)
 
-        print("Vamos Começar fale Alguma Coisa....")
+            print("Vamos Começar fale Alguma Coisa....")
 
-        audio = mic.listen(source=source)
+            audio = mic.listen(source=source)
 
-        try:
-            frase = mic.recognize_google(audio,language='pt-BR')
-            print("Você falou: "+ frase)
-        except sr.UnknownValueError:
-            print("Algo Deu errado")
+            try:
+                frase = mic.recognize_google(audio,language='pt-BR')
+                if re.search(r'\b'+"ajudar"+r'\b',format(frase)):
+                    print("Algo relacionado a ajuda.")
+                print("Você falou: "+ frase)
+            except sr.UnknownValueError:
+                print("Algo Deu errado")
