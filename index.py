7b14db5a2acaf974f47412dc381dcd9972ebb422
gesse-fernandes <gesse-fm@hotmail.com>
2023-01-27 10:14:43 -0300

    importando libs speech e chamando a IA da Google

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print("Olá gessex")
+import speech_recognition as sr
+
+mic = sr.Recognizer()
+
+with sr.Microphone() as source:
+        mic.adjust_for_ambient_noise(source=source)
+
+        print("Vamos Começar fale Alguma Coisa....")
+
+        audio = mic.listen(source=source)
+
+        try:
+            frase = mic.recognize_google(audio,language='pt-BR')
+            print("Você falou: "+ frase)
+        except sr.UnknownValueError:
+            print("Algo Deu errado")
